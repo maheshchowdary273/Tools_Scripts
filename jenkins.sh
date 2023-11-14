2f80c484d1f8a47bcb3b1dd5ce636e4391c72ef2
mahesh chowdary markapuri <56768889+maheshchowdary273@users.noreply.github.com>
2023-11-14 16:34:00 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,5 @@
+sudo apt-get update -y
+
 sudo apt install openjdk-17-jre -y
 
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -5,5 +7,5 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
+  
 sudo apt-get install jenkins -y
